326fa73cf27610d7632086515af7690dc33a06ac
EzequielGnich <ezequielgnich@live.com>
2019-09-10 12:07:50 -0300

    Criada a estrutura do footer

--- frontend/src/components/templates/footer/footer.jsx
+++ frontend/src/components/templates/footer/footer.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 
 export default props => (
   <footer className="footer">
-    Rodapé
+    <span>Desenvolvido com <i className="fa fa-heart text-danger"></i>&nbsp;por&nbsp; 
+    <strong>Ezequiel<i class="fa fa-code text-danger"></i>Gnich </strong>
+    
+    </span>
   </footer>
 )
