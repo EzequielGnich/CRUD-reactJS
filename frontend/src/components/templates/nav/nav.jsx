44fdc6fe3c99705c55e433c84b6d302d3e52eb5a
EzequielGnich <ezequielgnich@live.com>
2019-09-10 12:09:09 -0300

    Criada a estrutura de navegação do menu lateral da aplicação

--- frontend/src/components/templates/nav/nav.jsx
+++ frontend/src/components/templates/nav/nav.jsx
@@ -3,6 +3,16 @@ import React from 'react'
 
 export default props => (
   <aside className="menu-area">
-    Navegação
+    <nav className="menu">
+    {/* Refatorar para um componente */}
+      <a href="#/">
+        <i className="fa fa-home"></i>
+        <label>&nbsp;Início</label>
+      </a>
+      <a href="#/users">
+        <i className="fa fa-users"></i>
+        <label>&nbsp;Usuários</label>
+      </a>
+    </nav>
   </aside>
 )
