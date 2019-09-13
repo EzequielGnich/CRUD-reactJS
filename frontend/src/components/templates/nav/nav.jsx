2757aa252fe33d309cb08e613f367e96862590b1
EzequielGnich <ezequielgnich@live.com>
2019-09-13 10:49:56 -0300

    Alterado a estratégia para Link

--- frontend/src/components/templates/nav/nav.jsx
+++ frontend/src/components/templates/nav/nav.jsx
@@ -1,18 +1,19 @@
 import './nav.css'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default props => (
   <aside className="menu-area">
     <nav className="menu">
     {/* Refatorar para um componente */}
-      <a href="#/">
+      <Link to="/">
         <i className="fa fa-home"></i>
         <label>&nbsp;Início</label>
-      </a>
-      <a href="#/users">
+      </Link>
+      <Link to="/users">
         <i className="fa fa-users"></i>
         <label>&nbsp;Usuários</label>
-      </a>
+      </Link>
     </nav>
   </aside>
 )
