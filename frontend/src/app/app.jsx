59d2ab7b200399e69078ffaeec5d4ebed4b47ab9
EzequielGnich <ezequielgnich@live.com>
2019-09-13 10:46:57 -0300

    Alterado a chamada da home para Routes.jsx

--- frontend/src/app/app.jsx
+++ frontend/src/app/app.jsx
@@ -2,17 +2,20 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import "./app.css";
 import React from "react";
+import { HashRouter } from 'react-router-dom'
 
+import Routes from "./routes"
 import Logo from '../components/templates/logo/logo'
 import Nav from '../components/templates/nav/nav'
-import Main from '../components/templates/main/main'
 import Footer from '../components/templates/footer/footer'
 
 export default props => (
-  <div className="app">
-    <Logo />
-    <Nav />
-    <Main icon="home" title="Inicio" subtitle="Projeto Crud react!" />
-    <Footer />
-  </div>
+  <HashRouter>
+    <div className="app">
+      <Logo />
+      <Nav />
+      <Routes />
+      <Footer />
+    </div>
+  </HashRouter>
 )
