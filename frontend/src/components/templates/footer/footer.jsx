27ace827068bd5f09b677b1e66871bbb66421f29
EzequielGnich <ezequielgnich@live.com>
2019-09-13 12:07:57 -0300

    alterado class para className

--- frontend/src/components/templates/footer/footer.jsx
+++ frontend/src/components/templates/footer/footer.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default props => (
   <footer className="footer">
     <span>Desenvolvido com <i className="fa fa-heart text-danger"></i>&nbsp;por&nbsp; 
-    <strong>Ezequiel<i class="fa fa-code text-danger"></i>Gnich </strong>
+    <strong>Ezequiel<i className="fa fa-code text-danger"></i>Gnich </strong>
     
     </span>
   </footer>
