6ab853b35dd0cfbb216b4314774613adb4bb0fe8
EzequielGnich <ezequielgnich@live.com>
2019-09-13 10:49:17 -0300

    container-fluid, props.children

--- frontend/src/components/templates/main/main.jsx
+++ frontend/src/components/templates/main/main.jsx
@@ -6,8 +6,10 @@ import Header from '../header/header'
 export default props => (
   <>
     <Header {...props} />
-    <main className="content">
-      Conteúdo
+    <main className="content container-fluid">
+      <div className="p-3 mt-3">
+        {props.children}
+      </div>
     </main>
   </>
 )
